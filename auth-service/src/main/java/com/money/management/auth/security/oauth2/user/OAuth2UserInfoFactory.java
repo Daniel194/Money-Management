415452ecc78e6a07cf97c11f45b497c6610cc1fc
Daniel194 <lungu.daniel94@gmail.com>
2019-02-23 18:19:49 +0200

    auth-service -> OAuth2 Users

---
+++ auth-service/src/main/java/com/money/management/auth/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -0,0 +1,25 @@
+package com.money.management.auth.security.oauth2.user;
+
+import com.money.management.auth.domain.AuthProvider;
+import com.money.management.auth.exception.OAuth2AuthenticationProcessingException;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+
+        switch (AuthProvider.valueOf(registrationId)) {
+            case GOOGLE:
+                return new GoogleOAuth2UserInfo(attributes);
+            case FACEBOOK:
+                return new FacebookOAuth2UserInfo(attributes);
+            case TWITTER:
+                //TODO
+                return null;
+            default:
+                throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+        }
+    }
+
+}
