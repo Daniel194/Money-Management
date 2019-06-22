28dc4d671078b65e8465ea6ab40f1815763ae108
Daniel194 <lungu.daniel94@gmail.com>
2019-06-22 21:39:32 +0300

    cleanup

--- statistics-service/src/main/java/com/money/management/statistics/client/ExchangeRatesClient.java
+++ statistics-service/src/main/java/com/money/management/statistics/client/ExchangeRatesClient.java
@@ -1,11 +1,9 @@
 package com.money.management.statistics.client;
 
-import com.money.management.statistics.domain.Currency;
 import com.money.management.statistics.domain.ExchangeRatesContainer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(url = "${rates.url}", name = "rates-client")
 public interface ExchangeRatesClient {
